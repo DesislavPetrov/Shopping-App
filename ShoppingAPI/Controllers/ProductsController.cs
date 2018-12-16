50b6d9d410edce688a41b8d93cb84e2db4085e22
unknown <petrov_desislav@abv.bg>
2018-12-16 23:30:06 +0200

    First commit

---
+++ ShoppingAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ShoppingAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name = "Amazon Kindle Paperwhite", Category = "E-Readers", Price = 100 },
+            new Product {Id = 2, Name = "iPad Pro 11", Category = "Tablets", Price = 900 },
+            new Product {Id = 1, Name = "Kobo Aura H2O ", Category = "E-Reader", Price = 175 },
+            new Product {Id = 1, Name = "Samsung Galaxy Tab S4", Category = "Tablets", Price = 700 },
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
